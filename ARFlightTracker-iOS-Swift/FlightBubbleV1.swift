c128b80e73a404dea8fbb9fba9213e7795df1a60
Sanjeev Ghimire <sanjeevghimire@Sanjeevs-MacBook-Pro.local>
2017-06-06 10:48:47 -0700

    fixed issue of missing file on xcode after cloning

---
+++ ARFlightTracker-iOS-Swift/FlightBubbleV1.swift
@@ -0,0 +1,21 @@
+//
+//  FlightBubbleV1.swift
+//  IBMFlightTracker
+//
+//  Created by Sanjeev Ghimire on 2/15/17.
+//  Copyright © 2017 Sanjeev Ghimire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class FlightBubbleV1:UIView {
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+
+    @IBOutlet weak var flightImage: UIImageView!
+    @IBOutlet weak var flightName: UILabel!
+    
+    
+}
